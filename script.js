c9ebe07e8ad5409f5170175189dcb60844ab26bb
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-22 19:17:25 -0500

    funcionalidad para obtener los intentos cuando se seleccione el nivel de dificultad

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let numeroComputadora = Math.round(Math.random() * 100)+1
+let numeroComputadora = Math.round(Math.random() * 100) + 1
 
 let numeroUsuario = document.getElementById('numero_entrada')
 
 let mensaje = document.getElementById('mensaje')
+let mensajeNumeroIntentos = document.getElementById('mensajeNumeroIntentos')
 
+let opcionSeleccionada = document.getElementById('opciones')
+let numeroIntentos = 0
+
+function actualizarIntentos() {
+
+    switch (opcionSeleccionada.value) {
+        case 'op01':
+            numeroIntentos = 10;
+            break;
+        case 'op02':
+            numeroIntentos = 5;
+            break;
+        case 'op03':
+            numeroIntentos = 3;
+            break;
+    }
+
+    // Actualizar el texto en la página
+    mensajeNumeroIntentos.textContent = `Tienes ${numeroIntentos} intentos`;
+}
+
+
+opcionSeleccionada.addEventListener('change', actualizarIntentos);
 
 function verificarResultado() {
     let numeroIngresado = parseInt(numeroUsuario.value)
@@ -20,8 +44,16 @@ function verificarResultado() {
         // return
     } else if (numeroIngresado < numeroComputadora) {
         mensaje.textContent = 'El numero debe ser mayor'
+        numeroIntentos--
     } else {
         mensaje.textContent = 'El numero debe ser menor'
+        numeroIntentos--
+    }
+
+    if (numeroIntentos === 0) {
+        mensaje.textContent = `Perdiste! El número era ${numeroComputadora}`
+        mensaje.style.color = 'red'
+        numeroUsuario.disabled = true
     }
 
     console.log("clik")
